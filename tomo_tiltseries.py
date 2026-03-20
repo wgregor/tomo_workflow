881f044d82561852057ca8a0b310a4fbea864207
wgregor <wdg31@case.edu>
2026-03-20 17:59:47 -0400

    add basic functionality to run e2ddd_external.py for each raw tilt set imported with tomo_prepare_workspace.py

--- tomo_tiltseries.py
+++ tomo_tiltseries.py
@@ -3,14 +3,36 @@
 from argparse import ArgumentParser
 from subprocess import check_output, run
 import os
-
-#TEST CHANGE
-
-def get_set_nums(parent_directory):
-    files = check_output(['ls', parent_directory])
+import json
 
 def main():
-    print()
+    parser = ArgumentParser(description='prepares unaligned tiltseries for all sets of tilt images')
+    parser.add_argument('--apix', default=1.37, help='pixel spacing for raw tifs')
+    parser.add_argument('--flip_gain', default=2, type=int,
+                        help='how to flip the gain file. 0=none 1=flip-x 2=flip-y')
+
+    args=parser.parse_args()
+
+    with open('./tomo_info/info.json', 'r') as j_file:
+        info_dict = json.load(j_file)
+    directories = info_dict['directory_paths']
+    parent_directory = info_dict['parent_directory']
+
+    gain = os.path.join(parent_directory, 'gain.mrc')
+    if not os.path.isfile(gain):
+        print('invalid gain file provided')
+        exit()
+    defect = os.path.join(parent_directory, 'defects.txt')
+    if not os.path.isfile(defect):
+        print('invalid defect file provided')
+        exit()
+    for directory in directories:
+        base_name = directory.split('/')[-1]
+        raw_tif_path = os.path.join(directory, 'raw_tif')
+        mdoc_path = os.path.join(raw_tif_path, base_name + '.mdoc')
+        run(['e2ddd_external.py', raw_tif_path, '--program=ucsf_motioncor2', '--device=gpu', '--mdoc=' + mdoc_path,
+             '--apix=' + str(args.apix), '--gain=' + gain, '--defect_file=' + defect, '--mc2_rotgain=0',
+             '--mc2_flipgain='+str(args.flip_gain), '--imod_rotflipgain=0', '--device_num=0', '--tomo'])
 
 if __name__=='__main__':
     main()
